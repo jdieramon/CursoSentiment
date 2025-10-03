a78f50de560c2c9215ec6b786c9e31b6d58740a8
jdieramon <jodiera@upv.es>
2025-10-03 13:14:15 +0200

    add basics + tidyverse

---
+++ 0intro_basics.R
@@ -0,0 +1,43 @@
+## =============================================================================
+###  TEMA:    Fundamentos Básicos de R
+## =============================================================================
+
+
+# Vectores ---------------------------------------------------------------------
+
+saludo <- "Hola Mundo!"
+saludo
+print(saludo)
+
+x <- 3
+x
+
+# Las líneas que comienzan con # no son evaluadas. 
+# Se usan para hacer comentarios como este.    
+
+# Elevar al cuadrado
+x <- 3
+x**2
+
+# Elevar x a su valor 
+x**x
+
+# Propiedades de vectores: tipo/clase y tamaño/longitud
+# Tipo, se obtiene con 'typeof()'
+class(saludo)
+class(x)
+
+# Longitud, se obtiene con 'length()'
+length(saludo)
+length(x)
+
+# Definir el vector caracter 'ciudades' 
+ciudades <- c("Sevilla", "Granada", "Lugo")
+ciudades
+class(ciudades)
+length(ciudades)
+
+
+
+
+
---
+++ 0intro_tidyverse.R
@@ -0,0 +1,250 @@
+## =============================================================================
+###  TEMA:    Introducción a tablas de datos y tidyverse
+## =============================================================================
+
+## Limpiar el entorno de trabajo
+#rm(list=ls())
+
+## Cargar los paquetes necesarios para este tema
+#install.packages("gapminder")
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+
+# Ver el contenidpo de gapminder
+gapminder
+
+
+
+## FILTER
+## -----------------------------------------------------------------------------
+## Filtrar por una o varias condiciones
+gapminder %>%
+  filter(year == 2007)
+
+gapminder %>% 
+  filter(country == "Spain")
+
+gapminder %>% 
+  filter(year == 2007, country == "Spain")
+
+
+
+## ARRANGE
+## -----------------------------------------------------------------------------
+gapminder %>% 
+  arrange(gdpPercap)
+
+gapminder %>% 
+  arrange(desc(gdpPercap))
+
+# Combinar filter + arrange
+gapminder %>% 
+  filter(year == 2007) %>% 
+  arrange(desc(gdpPercap))
+
+
+
+## MUTATE
+## -----------------------------------------------------------------------------
+# Crear nueva variable
+gapminder %>% 
+  mutate(gdp = gdpPercap * pop)
+
+
+# Manipular variable existente
+gapminder %>% 
+  mutate(pop = pop/1e6) 
+
+
+# Combinar 3 verbos: filter + arrange + mutate
+gapminder %>% 
+  mutate(gdp = gdpPercap * pop) %>%
+  filter(year == 2007) %>% 
+  arrange(desc(gdp))
+
+
+
+## SUMMARIZE
+## -----------------------------------------------------------------------------
+# Resumir 1 variable
+gapminder %>%
+  summarize(meanLifeExp = mean(lifeExp))
+
+
+# Resumir 1 variable para un año
+gapminder %>% 
+  filter(year == 2007) %>% 
+  summarize(meanLifeExp = mean(lifeExp))
+
+
+# Resumir varias variables para un año
+gapminder %>% 
+  filter(year == 2007) %>% 
+  summarize(meanLifeExp = mean(lifeExp), 
+            totalPop = sum(as.numeric(pop)))
+
+
+# Resumir varias variables para un año con el total de observaciones: n()
+gapminder %>% 
+  filter(year == 2007) %>% 
+  summarize(totalCase = n(), 
+            meanLifeExp = mean(lifeExp), 
+            totalPop = sum(as.numeric(pop)))
+
+
+
+## GROUP_BY
+## -----------------------------------------------------------------------------
+# Agrupando por year
+gapminder %>% 
+  group_by(year)  %>% 
+  summarize(meanLifeExp = mean(lifeExp),totalPop = sum(as.numeric(pop)), 
+            maxGdpPercap = max(gdpPercap))
+
+
+# Agrupando por year con numero de casos n()
+gapminder %>% 
+  group_by(year)  %>% 
+  summarize(totalcase = n(), 
+            meanLifeExp = mean(lifeExp),totalPop = sum(as.numeric(pop)), 
+            maxGdpPercap = max(gdpPercap))
+
+
+# Agrupando por continente en 2007
+gapminder %>% 
+  filter(year == 2007) %>% 
+  group_by(continent)  %>% 
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(as.numeric(pop)),
+            maxGdpPercap = max(gdpPercap))
+
+
+# Agrupando por continente, year
+gapminder %>% 
+  group_by(year, continent)  %>% 
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(as.numeric(pop)),
+            maxGdpPercap = max(gdpPercap))
+
+
+
+
+## -----------------------------------------------------------------------------
+## VISUALIZACION DATOS: ggplot2
+## -----------------------------------------------------------------------------
+
+## Crear dataset año 2007 y asignarlo a 'gapminder_2007'
+gapminder_2007 <- gapminder %>% 
+  filter(year == 2007)
+
+
+## Scatter - plot
+## -----------------------------------------------------------------------------
+ggplot(gapminder_2007, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point()
+
+
+# Escala logaritmica
+ggplot(gapminder_2007,  aes(x = gdpPercap, y = lifeExp)) +
+  geom_point() + 
+  scale_x_log10()
+
+
+# Estetica adicional : color + tamaño
+## -----------------------------------------------------------------------------
+# variable categorica: color
+ggplot(gapminder_2007,  aes(x = gdpPercap, y = lifeExp, 
+                            color = continent)) +
+  geom_point() + 
+  scale_x_log10()
+
+
+## Change color
+ggplot(gapminder_2007,  aes(x = gdpPercap, y = lifeExp, 
+                            color = continent)) +
+  geom_point() + 
+  scale_x_log10() +
+  scale_color_brewer(palette="Dark2")
+
+## Change color2
+ggplot(gapminder_2007,  aes(x = gdpPercap, y = lifeExp, 
+                            color = continent)) +
+  geom_point() + 
+  scale_x_log10() +
+  scale_color_brewer(palette="Pastel2")
+
+
+## Change color3
+ggplot(gapminder_2007,  aes(x = gdpPercap, y = lifeExp, 
+                            color = continent)) +
+  geom_point() + 
+  scale_x_log10() +
+  scale_color_brewer(palette="PuOr")
+
+
+
+
+# variable numerica: tamaño
+ggplot(gapminder_2007,  
+       aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
+  geom_point() + 
+  scale_x_log10()
+
+
+# Paneles
+## -----------------------------------------------------------------------------
+ggplot(gapminder_2007,  aes(x = gdpPercap, y = lifeExp)) +
+  geom_point() + 
+  scale_x_log10() + 
+  facet_wrap(~ continent)
+
+
+# Datos resumidos
+## -----------------------------------------------------------------------------
+# Agrupando por continente, year
+by_year_continent <- gapminder %>% 
+  group_by(year, continent)  %>% 
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(as.numeric(pop)),
+            maxGdpPercap = max(gdpPercap))
+  
+
+ggplot(by_year_continent, 
+       aes(x = year, y= totalPop, color = continent)) +
+  geom_point()
+
+
+ggplot(by_year_continent, aes(x = year, y= totalPop, color = continent)) +
+  geom_point() + 
+  expand_limits(y = 0) # ejeY comienza desde 0
+
+
+## Line plot
+## -----------------------------------------------------------------------------
+gapminder %>% 
+  group_by(year) %>% 
+  summarise(totalPop = sum(as.numeric(pop))) %>%
+  ggplot(aes(x = year, y= totalPop)) + 
+           geom_line()
+
+
+# Datos resumidos
+ggplot(by_year_continent, 
+       aes(x = year, y= totalPop, color = continent)) +
+  geom_line()
+
+
+## Bar plot
+## -----------------------------------------------------------------------------
+# Agrupando por continente en 2007
+by_continent2007 <- gapminder %>% 
+  filter(year == 2007) %>% 
+  group_by(continent)  %>% 
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(as.numeric(pop)),
+            maxGdpPercap = max(gdpPercap))
+
+
+ggplot(by_continent2007, aes(x = continent, y = meanLifeExp)) + 
+  geom_col()
